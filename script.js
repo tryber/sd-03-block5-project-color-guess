3366d5cea337cc603f6b48b8d061021370b7d85e
Henrique Rezende <henriquerezendec@gmail.com>
2020-03-04 14:32:57 -0300

    Adicionando cores aleatorias

---
+++ script.js
@@ -0,0 +1,16 @@
+// Cria número de 0 a max
+const randomNumber = (max) => 0 + Math.floor((max - 0) * Math.random());
+
+// Cria cores aleatórias
+const randomColor = () => `(${randomNumber(255)} ,${randomNumber(255)} ,${randomNumber(255)})`;
+
+let answer = randomNumber(5)
+let answerElement = document.querySelectorAll(".ball")[answer]
+
+document.querySelectorAll(".ball").forEach(e => {
+  let randomClr = `rgb${randomColor()}`
+  e.style.backgroundColor = randomClr
+  if(e == answerElement){
+    document.getElementById("rgb-color").textContent = randomClr
+  }
+});
