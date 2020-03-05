e7c9de23a67b17f830337f1921bbd3d420bf8752
fabemiliano <fab.emiliano@gmail.com>
2020-03-05 08:50:01 -0300

    new pull request

---
+++ script.js
@@ -0,0 +1,41 @@
+let a = Math.floor(Math.random() * Math.floor(256))
+
+let b = Math.floor(Math.random() * Math.floor(256))
+
+let c = Math.floor(Math.random() * Math.floor(256))
+
+let randomRGB = `(${a},${b},${c})`
+
+document.getElementById("rgb-color").innerHTML = randomRGB
+document.getElementById("answer").innerHTML = "Escolha uma cor"
+
+let balls = document.querySelectorAll(".ball")
+
+for (let i = 0; i < 6; i++){
+    let a = Math.floor(Math.random() * Math.floor(256))
+
+    let b = Math.floor(Math.random() * Math.floor(256))
+
+    let c = Math.floor(Math.random() * Math.floor(256))
+
+    balls[i].innerHTML = `(${a},${b},${c})`
+    balls[i].style.backgroundColor= `rgb(${a},${b},${c})`
+}
+
+    let d = Math.floor(Math.random() * Math.floor(5))
+    balls[d].style.backgroundColor = `rgb(${a},${b},${c})`
+    balls[d].innerHTML = `(${a},${b},${c})`
+    
+for (let i = 0; i < 6; i++){
+    balls[i].addEventListener("click", giveAnswer)
+
+
+function giveAnswer(){
+
+   if(balls[i].innerHTML = randomRGB){
+    document.getElementById("answer").innerHTML = "Acertou"
+   }else{
+    document.getElementById("answer").innerHTML = "Errou" 
+   }
+}
+}
