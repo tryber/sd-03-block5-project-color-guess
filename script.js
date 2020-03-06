7b83ca65306e7c515e20f23fe1ac80623fb58d00
fabemiliano <fab.emiliano@gmail.com>
2020-03-06 08:01:13 -0300

    fixing code climate

--- script.js
+++ script.js
@@ -1,91 +1,86 @@
-let cont = Number(localStorage.getItem("contador")) //carregar o contador ao abrir a pagina
+let cont = Number(localStorage.getItem(contador)) // carregar o contador ao abrir a pagina
 
 function randomize() {
-    let a = Math.floor(Math.random() * Math.floor(256))
-    let b = Math.floor(Math.random() * Math.floor(256))
-    let c = Math.floor(Math.random() * Math.floor(256))
-    let randomRGB = `(${a},${b},${c})`
-    return randomRGB
+  const a = Math.floor(Math.random() * Math.floor(256));
+  const b = Math.floor(Math.random() * Math.floor(256));
+  const c = Math.floor(Math.random() * Math.floor(256));
+  const randomRGB = `(${a},${b},${c})`
+  return randomRGB
 }
 
-//styles
-randomRGB = randomize()
-styleText = randomize()
-styleboard = randomize()
-document.body.style.backgroundColor = `rgb${randomRGB}`
-document.getElementById("rgb-color").style.color= `rgb${styleText}`
-document.getElementById("title").style.color= `rgb${styleText}`
-document.getElementById("answer").style.color= `rgb${styleText}`
-document.getElementById("answer").style.fontSize= "30px"
-document.getElementById("placar").style.color= `rgb${styleText}`
-document.getElementById("score").style.color= `rgb${styleText}`
-document.getElementById("center").style.backgroundColor= `rgb${styleboard}`
+// styles    
+const randomRGB = randomize();
+const styleText = randomize();
+const styleboard = randomize();
+document.body.style.backgroundColor = `rgb${randomRGB}`;
+document.getElementById('rgb-color').style.color = `rgb${styleText}`;
+document.getElementById('title').style.color = `rgb${styleText}`;
+document.getElementById('answer').style.color = `rgb${styleText}`;
+document.getElementById('answer').style.fontSize = '30px';
+document.getElementById('placar').style.color = `rgb${styleText}`;
+document.getElementById('score').style.color = `rgb${styleText}`;
+document.getElementById('center').style.backgroundColor = `rgb${styleboard}`;
+
+document.getElementById('rgb-color').innerHTML = randomRGB;
+document.getElementById('answer').innerHTML = 'Escolha uma cor';
 
 
-
-document.getElementById("rgb-color").innerHTML = randomRGB
-document.getElementById("answer").innerHTML = "Escolha uma cor"
-
-
-let balls = document.querySelectorAll(".ball")
+const balls = document.querySelectorAll('.ball');
 
 for (let i = 0; i < 6; i++) {
-    randomRGBballs = randomize()
-
-    balls[i].innerHTML = randomRGBballs
+    const randomRGBballs = randomize();
+    balls[i].innerHTML = randomRGBballs;
     balls[i].style.backgroundColor = `rgb${randomRGBballs}`
 }
 
 
-let d = Math.floor(Math.random() * Math.floor(5))
-balls[d].style.backgroundColor = `rgb${randomRGB}`
-balls[d].innerHTML = randomRGB
-
+let d = Math.floor(Math.random() * Math.floor(5));
+balls[d].style.backgroundColor = `rgb${randomRGB}`;
+balls[d].innerHTML = randomRGB;
 
 
 for (let i = 0; i < 6; i++) {
-    balls[i].addEventListener("click", giveAnswer)
+    balls[i].addEventListener('click', giveAnswer);
 
     function giveAnswer() {
 
         if (balls[i].innerHTML == randomRGB) {
-            document.getElementById("answer").innerHTML = "Acertou!"
-            cont = cont + 3
-            localStorage.setItem("contador", cont)
-            document.getElementById("score").innerHTML = cont
+            document.getElementById('answer').innerHTML = 'Acertou!';
+            cont = cont + 3;
+            localStorage.setItem('contador', cont);
+            document.getElementById('score').innerHTML = cont
 
 
         } else {
-            document.getElementById("answer").innerHTML = "Errou! Tente novamente!"
-            cont = cont - 1
-            localStorage.setItem("contador", cont)
-            document.getElementById("score").innerHTML = cont
+            document.getElementById('answer').innerHTML = 'Errou! Tente novamente!';
+            cont = cont - 1;
+            localStorage.setItem('contador', cont);
+            document.getElementById('score').innerHTML = cont
         }
     }
 }
 
 
-let reset = document.getElementById("reset-game")
-reset.addEventListener("click", reload)
+let reset = document.getElementById('reset-game');
+reset.addEventListener('click', reload);
 
 function reload() {
     location.reload()
 }
 
-let restartAll = document.getElementById("reset-all")
-restartAll.addEventListener("click", restart)
+let restartAll = document.getElementById('reset-all');
+restartAll.addEventListener('click', restart);
 
 function restart() {
-    localStorage.clear()
+    localStorage.clear();
     location.reload()
 }
 
-document.getElementById("score").innerHTML = cont
+document.getElementById('score').innerHTML = cont;
 
-buttoncollor = randomize()
-textbuttoncollor = randomize()
-document.getElementsByTagName("button")[0].style.backgroundColor = `rgb${buttoncollor}`
-document.getElementsByTagName("button")[1].style.backgroundColor = `rgb${buttoncollor}`
-document.getElementsByTagName("button")[0].style.color = `rgb${textbuttoncollor}`
-document.getElementsByTagName("button")[1].style.color = `rgb${textbuttoncollor}`
-
+buttoncollor = randomize();
+textbuttoncollor = randomize();
+document.getElementsByTagName('button')[0].style.backgroundColor = `rgb${buttoncollor}`;
+document.getElementsByTagName('button')[1].style.backgroundColor = `rgb${buttoncollor}`;
+document.getElementsByTagName('button')[0].style.color = `rgb${textbuttoncollor}`;
+document.getElementsByTagName('button')[1].style.color = `rgb${textbuttoncollor}`;
