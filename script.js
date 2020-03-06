76d65ad2a18adf833c1518e2e35ec87f52dedc6d
fabemiliano <fab.emiliano@gmail.com>
2020-03-06 06:47:28 -0300

    more styles

--- script.js
+++ script.js
@@ -1,72 +1,88 @@
+let cont = Number(localStorage.getItem("contador")) //carregar o contador ao abrir a pagina
 
-let a = Math.floor(Math.random() * Math.floor(256))
+function randomize() {
+    let a = Math.floor(Math.random() * Math.floor(256))
+    let b = Math.floor(Math.random() * Math.floor(256))
+    let c = Math.floor(Math.random() * Math.floor(256))
+    let randomRGB = `(${a},${b},${c})`
+    return randomRGB
+}
 
-let b = Math.floor(Math.random() * Math.floor(256))
+//styles
+randomRGB = randomize()
+styleText = randomize()
+styleboard = randomize()
+document.body.style.backgroundColor = `rgb${randomRGB}`
+document.getElementById("rgb-color").style.color= `rgb${styleText}`
+document.getElementById("title").style.color= `rgb${styleText}`
+document.getElementById("answer").style.color= `rgb${styleText}`
+document.getElementById("answer").style.fontSize= "30px"
+document.getElementById("placar").style.color= `rgb${styleText}`
+document.getElementById("score").style.color= `rgb${styleText}`
+document.getElementById("center").style.backgroundColor= `rgb${styleboard}`
 
-let c = Math.floor(Math.random() * Math.floor(256))
 
-let randomRGB = `(${a},${b},${c})`
-
-let cont = Number(localStorage.getItem("contador"))
 
 document.getElementById("rgb-color").innerHTML = randomRGB
 document.getElementById("answer").innerHTML = "Escolha uma cor"
 
+
 let balls = document.querySelectorAll(".ball")
 
-for (let i = 0; i < 6; i++){
-    let a = Math.floor(Math.random() * Math.floor(256))
+for (let i = 0; i < 6; i++) {
+    randomRGB = randomize()
 
-    let b = Math.floor(Math.random() * Math.floor(256))
-
-    let c = Math.floor(Math.random() * Math.floor(256))
-
-    balls[i].innerHTML = `(${a},${b},${c})`
-    balls[i].style.backgroundColor= `rgb(${a},${b},${c})`
+    balls[i].innerHTML = randomRGB
+    balls[i].style.backgroundColor = `rgb${randomRGB}`
 }
 
-    let d = Math.floor(Math.random() * Math.floor(5))
-    balls[d].style.backgroundColor = `rgb(${a},${b},${c})`
-    balls[d].innerHTML = `(${a},${b},${c})`
-    
-for (let i = 0; i < 6; i++){
+let d = Math.floor(Math.random() * Math.floor(5))
+balls[d].style.backgroundColor = `rgb${randomRGB}`
+balls[d].innerHTML = randomRGB
+
+for (let i = 0; i < 6; i++) {
     balls[i].addEventListener("click", giveAnswer)
 
+    function giveAnswer() {
 
-function giveAnswer(){
-    
+        if (balls[i].innerHTML == randomRGB) {
+            document.getElementById("answer").innerHTML = "Acertou!"
+            cont = cont + 3
+            localStorage.setItem("contador", cont)
+            document.getElementById("score").innerHTML = cont
+            
 
-   if(balls[i].innerHTML == randomRGB){
-    document.getElementById("answer").innerHTML = "Acertou!"
-    cont = cont + 3
-    localStorage.setItem("contador", cont)
-    document.getElementById("score").innerHTML = cont
-    
-   }else{
-    document.getElementById("answer").innerHTML = "Errou! Tente novamente!" 
-    cont = cont - 1
-    localStorage.setItem("contador", cont)
-    document.getElementById("score").innerHTML = cont
-   }
-}
+        } else {
+            document.getElementById("answer").innerHTML = "Errou! Tente novamente!"
+            cont = cont - 1
+            localStorage.setItem("contador", cont)
+            document.getElementById("score").innerHTML = cont
+        }
+    }
 }
 
 
 let reset = document.getElementById("reset-game")
 reset.addEventListener("click", reload)
 
-function reload(){
+function reload() {
     location.reload()
 }
 
 let restartAll = document.getElementById("reset-all")
 restartAll.addEventListener("click", restart)
 
-function restart(){
+function restart() {
     localStorage.clear()
     location.reload()
 }
 
 document.getElementById("score").innerHTML = cont
 
+buttoncollor = randomize()
+textbuttoncollor = randomize()
+document.getElementsByTagName("button")[0].style.backgroundColor = `rgb${buttoncollor}`
+document.getElementsByTagName("button")[1].style.backgroundColor = `rgb${buttoncollor}`
+document.getElementsByTagName("button")[0].style.color = `rgb${textbuttoncollor}`
+document.getElementsByTagName("button")[1].style.color = `rgb${textbuttoncollor}`
 
