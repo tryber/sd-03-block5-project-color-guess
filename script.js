d4ee6423de1c3298a8b4f91308bac06d6308481f
Felipe Gomes <contato.felipeluna@gmail.com>
2020-03-10 08:04:47 -0300

    code climate 08.04

--- script.js
+++ script.js
@@ -1,43 +1,16 @@
-let ball = document.getElementsByClassName('ball');
 const iniciar = document.getElementById('reset-game');
 const answer = document.getElementById('answer');
 const score = document.getElementById('score');
 iniciar.addEventListener('click', paint);
 
 function randColor() {
-  let a = Math.floor(Math.random() * 256);
-  let b = Math.floor(Math.random() * 256);
-  let c = Math.floor(Math.random() * 256);
-  let color = "RGB(" + a + ", " + b + ", " + c + ")";
+  const a = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const c = Math.floor(Math.random() * 256);
+  const color = 'RGB('+a+', '+b+', '+c+')';
 
   return color;
 }
-
-function paint() {
-  score.innerHTML = 0;
-  answer.innerHTML = 'Escolha uma cor';
-  const rgb = document.getElementById('rgb-color');
-  let ball = document.getElementsByClassName('ball');
-
-  for (let i=0; i<ball.length; i++) {
-    ball[i].style.backgroundColor = randColor();
-  }
-  // for(let i in ball) {
-  //   ball[i].style.backgroundColor = randColor();
-  // }
-  let a = Math.floor(Math.random() * 5);
-  rgb.innerHTML = document.getElementsByClassName('ball')[a].style.backgroundColor;
-
-  for (let i=0; i<ball.length; i++) {
-    if (ball[i].style.backgroundColor == rgb.innerHTML) {
-      ball[i].addEventListener('click', acertou);
-    }else {
-      ball[i].addEventListener('click', errou);
-    }
-  }
-}
-
-paint();
 
 function acertou() {
   answer.innerHTML = 'Acertou!';
@@ -49,3 +22,30 @@ function acertou() {
 function errou() {
   answer.innerHTML = 'Errou! Tente novamente!';
 }
+
+function paint() {
+  score.innerHTML = 0;
+  answer.innerHTML = 'Escolha uma cor';
+  const rgb = document.getElementById('rgb-color');
+  const ball = document.getElementsByClassName('ball');
+
+  for (let i=0; i<ball.length; i++) {
+    ball[i].style.backgroundColor = randColor();
+  }
+  // for(let i in ball) {
+  //   ball[i].style.backgroundColor = randColor();
+  // }
+  const a = Math.floor(Math.random() * 5);
+  rgb.innerHTML = document.getElementsByClassName('ball')[a].style.backgroundColor;
+
+  for (let i=0; i<ball.length; i++) {
+    if (ball[i].style.backgroundColor === rgb.innerHTML) {
+      ball[i].addEventListener('click', acertou);
+    }else {
+      ball[i].addEventListener('click', errou);
+    }
+  }
+}
+
+paint();
+
