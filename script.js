523c51b698a57ba643dbf4d1e4ab32d9dfbd0d3e
Felipe Gomes <contato.felipeluna@gmail.com>
2020-03-10 00:06:35 -0300

    add score

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-function randcolor() {
+ball = document.getElementsByClassName('ball');
+iniciar = document.getElementById('start');
+answer = document.getElementById('answer');
+score = document.getElementById('score');
+iniciar.addEventListener('click', paint);
+
+function randColor() {
   let a = Math.floor(Math.random() * 256);
   let b = Math.floor(Math.random() * 256);
   let c = Math.floor(Math.random() * 256);
-  let color = "(" + a + ", " + b + ", " + c + ")";
-  rgb = document.getElementById('rgb-color');
-  rgb.innerHTML = color;
-
-  let x = Math.floor(Math.random() * 256);
-  let y = Math.floor(Math.random() * 256);
-  let z = Math.floor(Math.random() * 256);
-  let colorb = "rgb(" + x + ", " + y + ", " + z + ")";
-  ball = document.getElementsByClassName('ball');
-
-  for(let i in ball){
-    ball[i].style.backgroundColor = colorb;
-  }
-
+  let color = "RGB(" + a + ", " + b + ", " + c + ")";
+  
+  return color;
 }
 
-iniciar = document.getElementById('start');
-iniciar.addEventListener('click', randcolor);
+function paint(){
+  score.innerHTML = 0;
+  answer.innerHTML = 'Escolha uma cor';
+  rgb = document.getElementById('rgb-color');
+  ball = document.getElementsByClassName('ball');
+  
+  for (let i=0; i<ball.length; i++){
+    ball[i].style.backgroundColor = randColor();
+  }
+  // for(let i in ball) {
+  //   ball[i].style.backgroundColor = randColor();
+  // }
+  let a = Math.floor(Math.random() * 5);
+  rgb.innerHTML = document.getElementsByClassName('ball')[a].style.backgroundColor;
+
+  for (let i=0; i<ball.length; i++) {
+    if (ball[i].style.backgroundColor == rgb.innerHTML){
+      ball[i].addEventListener('click', acertou);
+    }else {
+      ball[i].addEventListener('click', errou);
+    }
+  }
+}
+
+paint();
+  
+function acertou(){
+  answer.innerHTML = 'Acertou!';
+  score.innerHTML++;
+  score.innerHTML++;
+  score.innerHTML++;
+}
+
+function errou(){
+  answer.innerHTML = 'Errou! Tente novamente!';
+}
