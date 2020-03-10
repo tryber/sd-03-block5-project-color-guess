db04a40bf3c52e186710072da95a55080687f3e2
LulDelRey <rpgstrikered@gmail.com>
2020-03-10 18:52:44 -0300

    correcting stuff again

--- script.js
+++ script.js
@@ -2,14 +2,10 @@ const answer = document.getElementById('answer');
 const score = document.getElementById('score');
 
 function randomColor() {
-  const color =
-    'rgb(' +
-    Math.floor(Math.random() * 256) +
-    ', ' +
-    Math.floor(Math.random() * 256) +
-    ', ' +
-    Math.floor(Math.random() * 256) +
-    ')';
+  const a = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const c = Math.floor(Math.random() * 256);
+  const color = `rgb(${a}, ${b}, ${c})`;
   return color;
 }
 
@@ -27,14 +23,14 @@ function startGame() {
   const rgbColor = document.getElementById('rgb-color');
   const balls = document.getElementsByClassName('ball');
 
-  for (let i = 0; i < balls.length; i++) {
+  for (let i = 0; i < balls.length; i += 1) {
     balls[i].style.backgroundColor = randomColor();
   }
 
   const randomAnswer = Math.floor(Math.random() * (6 - 1)) + 1;
   rgbColor.innerHTML = balls[randomAnswer].style.backgroundColor;
 
-  for (let i = 0; i < balls.length; i++) {
+  for (let i = 0; i < balls.length; i += 1) {
     if (balls[i].style.backgroundColor === rgbColor.innerHTML) {
       balls[i].addEventListener('click', correct);
     } else {
