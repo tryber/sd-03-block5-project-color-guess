f0357f6e465320456d28028e7f48cffec3b72628
Felipe Gomes <contato.felipeluna@gmail.com>
2020-03-10 08:16:18 -0300

    score fix

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 const iniciar = document.getElementById('reset-game');
 const answer = document.getElementById('answer');
 const score = document.getElementById('score');
-iniciar.addEventListener('click', paint);
 
 function randColor() {
   const a = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
   const c = Math.floor(Math.random() * 256);
-  const color = 'RGB('+a+', '+b+', '+c+')';
+  const color = 'RGB(' + a + ', ' + b + ', ' + c + ')';
 
   return color;
 }
 
 function acertou() {
   answer.innerHTML = 'Acertou!';
-  score.innerHTML++;
-  score.innerHTML++;
-  score.innerHTML++;
+  score.innerText = Number(score.innerText)+3;
 }
 
 function errou() {
   answer.innerHTML = 'Errou! Tente novamente!';
+  score.innerText = Number(score.innerText)-1;  
 }
 
 function paint() {
@@ -38,14 +36,14 @@ function paint() {
   const a = Math.floor(Math.random() * 5);
   rgb.innerHTML = document.getElementsByClassName('ball')[a].style.backgroundColor;
 
-  for (let i=0; i<ball.length; i++) {
+  for (let i = 0; i < ball.length; i++) {
     if (ball[i].style.backgroundColor === rgb.innerHTML) {
       ball[i].addEventListener('click', acertou);
-    }else {
+    } else {
       ball[i].addEventListener('click', errou);
     }
   }
 }
 
+iniciar.addEventListener('click', paint);
 paint();
-
