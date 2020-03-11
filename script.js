6fc8261091f47e02ee6309329b98578d2e9bb7d9
Felipe Gomes <contato.felipeluna@gmail.com>
2020-03-10 22:07:13 -0300

    score fix 10.07

--- script.js
+++ script.js
@@ -1,37 +1,22 @@
 const reset = document.getElementById('reset-game');
 const answer = document.getElementById('answer');
 const score = document.getElementById('score');
-let acerto = 0;
+const rgb = document.getElementById('rgb-color');
+const ball = document.getElementsByClassName('ball');
+let acertou = 0;
 
 function randColor() {
   const a = Math.floor(Math.random() * 256);
   const b = Math.floor(Math.random() * 256);
   const c = Math.floor(Math.random() * 256);
-  const color = 'RGB(' + a + ', ' + b + ', ' + c + ')';
+  const color = 'rgb(' + a + ', ' + b + ', ' + c + ')';
   
   return color;
 }
 
-function acertou() {
-  answer.innerHTML = 'Acertou!';
-  if (acerto == 0){
-    score.innerText = Number(score.innerText)+3;
-  } else {
-    console.log('ja clicou');
-  }
-  acerto = 1;
-}
-
-function errou() {
-  answer.innerHTML = 'Errou! Tente novamente!';
-  // score.innerText = Number(score.innerText)-1;
-}
-
 function paint() {
-  acerto = 0;
+  acertou = 0;
   answer.innerHTML = 'Escolha uma cor';
-  const rgb = document.getElementById('rgb-color');
-  const ball = document.getElementsByClassName('ball');
 
   for (let i=0; i<ball.length; i++) {
     ball[i].style.backgroundColor = randColor();
@@ -41,11 +26,16 @@ function paint() {
   rgb.innerHTML = document.getElementsByClassName('ball')[a].style.backgroundColor;
 
   for (let i = 0; i < ball.length; i++) {
-    if (ball[i].style.backgroundColor === rgb.innerHTML) {
-      ball[i].addEventListener('click', acertou);
-    } else {
-      ball[i].addEventListener('click', errou);
-    }
+      ball[i].addEventListener('click', function result(){
+        if (ball[i].style.backgroundColor === rgb.innerHTML) {
+          answer.innerHTML = 'Acertou!';
+          if (acertou == 0) {
+            score.innerText = Number(score.innerText)+3;
+          } acertou = 1;
+        } else {
+          answer.innerHTML = 'Errou! Tente novamente!';
+        }
+      });
   }
 }
 
