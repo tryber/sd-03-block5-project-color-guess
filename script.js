9f2c7a77f2e5eb16497e9c2ba14da2ab39862cc8
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-03-14 23:14:04 -0300

    code climate

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
-const balls = document.querySelectorAll(".ball");
-const arrayCores = ["", "", "", "", "", ""];
-const answer = document.querySelector("#answer");
-const rgbColor = document.querySelector("#rgb-color");
-const botao1 = document.querySelector("#botao1");
-const botao2 = document.querySelector("#botao2");
-const placar = document.querySelector("#score");
-const scoreSalvo = localStorage.getItem("Score");
+const balls = document.querySelectorAll('.ball');
+const arrayCores = ['', '', '', '', '', ''];
+const answer = document.querySelector('#answer');
+const rgbColor = document.querySelector('#rgb-color');
+const botao1 = document.querySelector('#botao1');
+const botao2 = document.querySelector('#botao2');
+const placar = document.querySelector('#score');
+const scoreSalvo = localStorage.getItem('Score');
 let score = 0;
-console.log(typeof(score));
 
 if (scoreSalvo) {
   score = parseInt(scoreSalvo);
-  console.log(typeof(score));
-} 
+}
 
 function corAleatoria() {
-  const hexadecimais = "0123456789ABCDEF";
-  let cor = "#";
+  const hexadecimais = '0123456789ABCDEF';
+  let cor = '#';
   for (let i = 0; i < 6; i += 1) {
     cor += hexadecimais[Math.floor(Math.random() * 16)];
   }
   return cor;
 }
 
-window.onload = function() {
+window.onload = function () {
   for (let i = 0; i < balls.length; i += 1) {
     balls[i].style.backgroundColor = corAleatoria();
     arrayCores[i] += balls[i].style.backgroundColor;
   }
   const cor = Math.round(Math.random() * (arrayCores.length - 1));
-  answer.innerHTML = "Escolha uma cor";
+  answer.innerHTML = 'Escolha uma cor';
   rgbColor.innerHTML = arrayCores[cor];
   placar.innerHTML = score;
 };
 
 function adivinhaCor(i) {
-  balls[i].addEventListener("click", function() {
-    if (balls[i].style.backgroundColor == rgbColor.innerHTML) {
-      answer.innerHTML = "Acertou!";
+  balls[i].addEventListener('click', function() {
+    if (balls[i].style.backgroundColor === rgbColor.innerHTML) {
+      answer.innerHTML = 'Acertou!';
       score += 3;
       placar.innerHTML = score;
-      localStorage.setItem("Score", score);
+      localStorage.setItem('Score', score);
     } else {
-      answer.innerHTML = "Errou! Tente novamente!";
+      answer.innerHTML = 'Errou! Tente novamente!';
     }
   });
 }
@@ -52,12 +50,12 @@ for (let i = 0; i < arrayCores.length; i += 1) {
 }
 
 function limpaPlacar() {
-    localStorage.clear();
-    placar.innerHTML = 0;
+  localStorage.clear();
+  placar.innerHTML = 0;
 }
 
-botao1.addEventListener("click", function() {
+botao1.addEventListener('click', function() {
   window.location.reload();
 });
 
-botao2.addEventListener("click", limpaPlacar);
+botao2.addEventListener('click', limpaPlacar);
