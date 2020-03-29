dd2ed7a9108c1fdb0ecc9328c22c5d97a5ba6ffe
wscatao <wscatao@gmail.com>
2020-03-29 11:55:53 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -2,55 +2,57 @@
 
 const parRgb = document.getElementById('rgb-color');
 const resposta = document.getElementById('answer');
-const divsBall = document.querySelectorAll('.ball')
+const divsBall = document.querySelectorAll('.ball');
+const botao = document.getElementById('botao');
 
 // Funções
 
-const checarResposta = divs => {
-  divs.forEach(element => {
+const checarResposta = (divs) => {
+  divs.forEach((element) => {
     element.addEventListener('click', () => {
       const teste = event.currentTarget.style.backgroundColor;
       const correto = parRgb.innerText;
-      if(teste === correto) {
-        console.log(resposta);
+      if (teste === correto) {
         resposta.innerHTML = 'Acertou!';
       } else {
         resposta.innerHTML = 'Errou! Tente novamente!';
       }
     });
   });
-}
+};
 
 const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-const sorteia = divs => {
-  sorteio = getRandomIntInclusive(0,5);
-  divs[sorteio].style.backgroundColor = `rgb${gerarCorAl()}`;
-  parRgb.innerHTML = divs[sorteio].style.backgroundColor;
-  for(let i = 0; i < divs.length; i += 1) {
-    if(divs[i] != divs[sorteio]) {
-      divs[i].style.backgroundColor = `rgb${gerarCorAl()}`;
-    } 
-  }
-}
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 const gerarCorAl = () => {
   const cor = `(${Math.floor((Math.random() * 255) + 1)}, ${Math.floor((Math.random() * 255) + 1)}, ${Math.floor((Math.random() * 255) + 1)})`;
   return cor;
 };
 
+const sorteia = (divs) => {
+  const sorteio = getRandomIntInclusive(0, 5);
+  divs[sorteio].style.backgroundColor = `rgb${gerarCorAl()}`;
+  parRgb.innerHTML = divs[sorteio].style.backgroundColor;
+  for (let i = 0; i < divs.length; i += 1) {
+    if (divs[i] !== divs[sorteio]) {
+      divs[i].style.backgroundColor = `rgb${gerarCorAl()}`;
+    }
+  }
+};
+
 const init = () => {
-  sorteiaBall = sorteia(divsBall);
+  sorteia(divsBall);
   resposta.innerHTML = 'Escolha uma cor';
 };
 
-// EventListerner 
-
 checarResposta(divsBall);
+
+// EventListener
+
+botao.addEventListener('click', init);
 
 // Load
 
