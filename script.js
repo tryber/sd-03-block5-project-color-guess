6a65dab1625ca16d4067e020c4dd64cac073a55d
wscatao <wscatao@gmail.com>
2020-03-29 10:57:01 -0300

    Requisitos 1,2 e 3 e CC

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 // Elementos
 
 const parRgb = document.getElementById('rgb-color');
+const resposta = document.getElementById('answer');
+// const containerBall = document.querySelector('.containerBall')
 
-//Funções
+// Funções
 
 const gerarCorAl = () => {
   const cor = `(${Math.floor((Math.random() * 255) + 1)}, ${Math.floor((Math.random() * 255) + 1)}, ${Math.floor((Math.random() * 255) + 1)})`;
-  return cor
-}
+  return cor;
+};
 
-//Inicialização 
+const init = () => {
+  parRgb.innerHTML = gerarCorAl();
+  resposta.innerHTML = 'Escolha uma cor';
+};
 
-window.onload(parRgb.innerHTML = gerarCorAl());
+window.onload(init());
